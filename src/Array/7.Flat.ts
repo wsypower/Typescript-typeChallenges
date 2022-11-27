cd2ae133086945add136ff09d9ecb0a99dbe5dc4
weiyafei <2553241022@qq.com>
2022-11-27 21:22:10 +0800

    📦 NEW: Flat

---
+++ src/Array/7.Flat.ts
@@ -0,0 +1,25 @@
+/*
+ * @Description: 
+ * @Author: wsy
+ * @Date: 2022-11-27 21:10:42
+ * @LastEditTime: 2022-11-27 21:21:59
+ * @LastEditors: wsy
+ */
+
+type A = Flat<[1, 2, 3]>; // [1,2,3]
+type B = Flat<[[[[[[[1]]]]]], [2, 3], [4, [5, [6]]]]>; // [1,2,3,4,5,6]
+type C = Flat<[]>; // []
+type D = Flat<[1]>; // [1]
+
+
+
+/**
+ * Flattens an array of arrays.           
+ * @param {any[]} arr - the array to flatten           
+ * @returns {any[]} - the flattened array           
+ */
+export type Flat<T extends any[]> = T extends [infer L, ...infer R]
+  ? L extends any[]
+  ? [...Flat<L>, ...Flat<R>]
+  : [L, ...Flat<R>]
+  : T
