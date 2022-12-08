a02757f61f31ed3e7759cbd3890222f0f62ae393
weiyafei <2553241022@qq.com>
2022-12-08 20:46:55 +0800

    Primitive

---
+++ src/util/type-fest.ts
@@ -0,0 +1,10 @@
+/*
+ * @Description: 
+ * @Author: wsy
+ * @Date: 2022-12-06 20:51:10
+ * @LastEditTime: 2022-12-08 20:40:02
+ * @LastEditors: wsy
+ */
+import type { Primitive } from 'type-fest'
+
+
