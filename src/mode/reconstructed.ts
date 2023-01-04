eddea6db910c65a56d156d6093062809cb2a967c
weiyafei <2553241022@qq.com>
2023-01-04 19:40:29 +0800

    📦 NEW: unShift

--- src/mode/reconstructed.ts
+++ src/mode/reconstructed.ts
@@ -2,9 +2,12 @@
  * @Description: 套路二 从新构造做转换
  * @Author: wsy
  * @Date: 2023-01-04 19:35:15
- * @LastEditTime: 2023-01-04 19:37:55
+ * @LastEditTime: 2023-01-04 19:39:10
  * @LastEditors: wsy
  */
 type Push<Arr extends unknown[], Element> = [...Arr, Element]
-
 type PushResult = Push<[1, 2, 3], 4> // [1,2,3,4]
+
+type UnShift<Arr extends unknown[], El> = [El, ...Arr]
+type UnShiftResult = UnShift<[1, 2, 3], 4> // [4,1,2,3]
+
