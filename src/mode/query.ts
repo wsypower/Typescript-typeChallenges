6f1fe21d766ba1f9f6f00fd97706d727db41d7f3
weiyafei <2553241022@qq.com>
2023-01-09 19:30:05 +0800

    parseQueryString

---
+++ src/mode/query.ts
@@ -0,0 +1,65 @@
+/*
+ * @Description: 
+ * @Author: wsy
+ * @Date: 2023-01-09 19:25:15
+ * @LastEditTime: 2023-01-09 19:29:08
+ * @LastEditors: wsy
+ */
+type ParseParam<Param extends string> =
+  Param extends `${infer Key}=${infer Value}`
+  ? {
+    [K in Key]: Value
+  } : Record<string, any>;
+
+type MergeValues<One, Other> =
+  One extends Other
+  ? One
+  : Other extends unknown[]
+  ? [One, ...Other]
+  : [One, Other];
+
+type MergeParams<
+  OneParam extends Record<string, any>,
+  OtherParam extends Record<string, any>
+> = {
+    readonly [Key in keyof OneParam | keyof OtherParam]:
+    Key extends keyof OneParam
+    ? Key extends keyof OtherParam
+    ? MergeValues<OneParam[Key], OtherParam[Key]>
+    : OneParam[Key]
+    : Key extends keyof OtherParam
+    ? OtherParam[Key]
+    : never
+  }
+
+type ParseQueryString<Str extends string> =
+  Str extends `${infer Param}&${infer Rest}`
+  ? MergeParams<ParseParam<Param>, ParseQueryString<Rest>>
+  : ParseParam<Str>;
+
+
+function parseQueryString<Str extends string>(queryStr: Str): ParseQueryString<Str>;
+function parseQueryString(queryStr: string) {
+  if (!queryStr || !queryStr.length) {
+    return {};
+  }
+  const queryObj: Record<string, any> = {};
+  const items = queryStr.split('&');
+  items.forEach(item => {
+    const [key, value] = item.split('=');
+    if (queryObj[key]) {
+      if (Array.isArray(queryObj[key])) {
+        queryObj[key].push(value);
+      } else {
+        queryObj[key] = [queryObj[key], value]
+      }
+    } else {
+      queryObj[key] = value;
+    }
+  });
+  return queryObj;
+}
+
+
+const res = parseQueryString('a=1&b=2&c=3');
+const res1 = parseQueryString('');
